16976bb80792e8c04a8121cee71200642f161432
mamoun alrefai <mamounalrefai6@gmail.com>
2024-10-06 16:51:46 +0300

    dd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: process.env.PORT || 3000, // Use the port provided by Render or fallback to 3000 for local development
+  },
 })
