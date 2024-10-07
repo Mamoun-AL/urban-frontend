2dfd253b463c48ef27e87393ddbd0c0876cc910f
mamoun alrefai <77227904+Mamoun-AL@users.noreply.github.com>
2024-10-07 12:08:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: '0.0.0.0', // Bind to all network interfaces
-    port: process.env.PORT || 3000, // Use the port provided by Render or fallback to 3000 for local development
+  build: {
+    outDir: 'dist', // Ensure this is the correct output directory
+  },
+  server: {
+    host: '0.0.0.0', // For local development, but Render will use this internally
+    port: process.env.PORT || 3000,
   },
 })
